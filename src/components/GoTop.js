ee59ff7b000b38e12ac2e8f824774b0f419ccc39
Park Sieun <pmj8856@naver.com>
2023-02-21 15:06:15 +0900

    Anime.js,  AOS 적용, 모바일 버튼 등 작업

---
+++ src/components/GoTop.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from "react";
+import Anime from "../assets/Anime";
+
+const GoTop = () => {
+  const style = {
+    position: "fixed",
+    right: 50,
+    bottom: -200,
+    width: 50,
+    height: 50,
+    background: "hotpink",
+    zIndex: 9999999,
+    cursor: "pointer",
+    fontSize: 0,
+    transition: "bottom 0.5s",
+  };
+  // 스크롤 위치 값 파악하기
+  const bt = useRef(null);
+  const scrollFN = () => {
+    // 스크롤 값 파악하여서 if 문 사용하기
+    const scY = window.document.documentElement.scrollTop;
+    // console.log(scY);
+    if (scY > 300) {
+      bt.current.classList.add("active");
+    } else {
+      bt.current.classList.remove("active");
+    }
+  };
+  useEffect(() => {
+    scrollFN();
+    window.addEventListener("scroll", scrollFN);
+    return () => {
+      window.removeEventListener("scroll", scrollFN);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={bt}
+      style={style}
+      onClick={() => {
+        new Anime(window, {
+          prop: "scroll",
+          value: 0,
+          duration: 500,
+        });
+      }}
+    >
+      GoTop
+    </div>
+  );
+};
+
+export default GoTop;
